d3d5ce2d7ee85f3beab822a352b1f75973e7a729
Stephen Radford <steve2288uk@gmail.com>
2012-06-24 20:31:48 +0100

    Fixed search form

--- blog/sidebar.php
+++ blog/sidebar.php
@@ -1,5 +1,5 @@
 <h4>Search</h4>
-<form method="get" action="<?php echo $_GET['page'] ?>/search">
+<form method="get" action="<?php echo URL_PATH.$_GET['page'] ?>/search">
 	<input type="text" name="q" placeholder="Search" id="search-box" />
 </form>
 

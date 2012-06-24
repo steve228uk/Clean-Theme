8dd37508d401b8adbdf1942785a13c77c26c2882
Stephen Radford <steve2288uk@gmail.com>
2012-06-24 19:45:32 +0100

    Updated 404 and list styles

--- 404.php
+++ 404.php
@@ -1,2 +1,8 @@
-<h1>Error 404 - Page Not Found</h1>
+<?php
+
+$errors = array('Oh no! ', 'Meeser Peter, ', 'Kaboom! ', 'Ooopsie! ', 'Our Bad, ');
+shuffle($errors) ?>
+
+<h1><?php echo $errors[0] ?>Error 404 - Page Not Found</h1>
+<p>Were you looking for one of these perhaps?</p>
 <?php theNav('sitemap') ?>
